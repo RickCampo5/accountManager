128d2fd7ebc9da78fdd7fb4e2d11e78b60570a08
Ricardo Campos Martinez <ricamposm@MacBook-Air-de-Ricardo.local>
2020-02-01 22:53:34 -0600

    connected to mongo

---
+++ app.js
@@ -0,0 +1,62 @@
+require('dotenv').config
+
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const hbs = require('hbs')
+const mongoose = require('mongoose')
+const path = require('path')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+
+mongoose.Promise = Promise
+
+mongoose.connect('mongodb://localhost/accountManager', {useNewUrlParser: true})
+  .then (() => {
+    console.log('Connected to Mongo')
+  }).catch(error => {
+    console.error('Error connecting to mongo', error)
+  })
+
+
+const app = express()
+app.use(require('cors')({
+  origin: true,
+  credentials: true
+}))
+
+app.use(session({
+  store: new MongoStore({
+    mongooseConnection:mongoose.connection,
+    ttl:24*60*60
+  }),
+  secret: 'Rick',
+  saveUninitialized: true,
+  resave: false,
+  cookie : { httpOnly: true, maxAge: 2419200000 }
+}))
+
+// Middleware Setup
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+//passport initilize
+// const passport = require('./helpers/passport')
+// app.use(passport.initialize())
+// app.use(passport.session())
+
+//Set views
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//Routes
+const index = require('./routes/index')
+app.use('/', index)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+
+module.exports = app
